d56c211f4f4a74d19d0d3beacf54f514505b20fa
Andrea Bertagnolli <andrea.bertagnolli@paros.srl>
2016-11-12 11:22:33 +0100

    Test refactor: extract setup method

--- src/test/java/ndr/brt/GameTest.java
+++ src/test/java/ndr/brt/GameTest.java
@@ -1,15 +1,21 @@
 package ndr.brt;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class GameTest {
 
+    private Game game;
+
+    @Before
+    public void setUp() throws Exception {
+        game = new Game();
+    }
+
     @Test
     public void twenty_miss_rolls() throws Exception {
-        Game game = new Game();
-
         for (int i = 0; i < 20; i++) {
             game.roll(0);
         }
@@ -19,12 +25,11 @@ public class GameTest {
 
     @Test
     public void twenty_rolls_that_scores_one() throws Exception {
-        Game game = new Game();
-
         for (int i = 0; i < 20; i++) {
             game.roll(1);
         }
 
         assertEquals(20, game.score());
     }
+
 }
